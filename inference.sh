ebbcc9314bf1a3565d591c44a107b2f87567770e
LiaoQian <cheer-me@outlook.com>
2019-11-27 10:58:47 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,8 +1,9 @@
-CUDA_VISIBLE_DEVICES=1 python main.py \
+python main.py \
     --output_dir ./results/ \
     --log_dir ./results/ \
     --content_dir ./contents/ \
     --mode inference \
     --pre_trained True \
     --checkpoint ./starry-night_in/model-40000 \
-    --normalizer in
+    --normalizer in \
+    --vgg_ckpt ./vgg19/vgg_19.ckpt
